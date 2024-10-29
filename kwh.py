00dc57a1c4a8bbc2b54d23588b015377352d5086
riennz <kiyarien1001@gmail.com>
2024-10-29 19:43:10 +0700

    Update kwh.py

--- kwh.py
+++ kwh.py
@@ -2,15 +2,13 @@ a = int(input('masukan lama pemakaian tv perjam :'))
 b = int(input('masukan lama pemakaian kipas perjam :'))
 c = int(input('masukan lama pemakaian lampu perjam :'))
 
-harga = 1500
+tv = int(input('masukan daya tv :'))
+kipas = int(input('masukan daya kipas :'))
+lampu = int(input('masukan daya lampu : :'))
 
-tv = 15
-kipas = 10
-lampu = 5
-
-total_pemakaian_tv = tv * a
-total_pemakaian_kipas = kipas * b
-total_pemakaian_lampu = lampu * c
+total_pemakaian_tv = tv * a / 1000
+total_pemakaian_kipas = kipas / 1000
+total_pemakaian_lampu = lampu / 1000
 
 print('total pemakaian per jam: ', total_pemakaian_tv)
 print('total pemakaian per jam: ', total_pemakaian_kipas)
@@ -20,14 +18,10 @@ total_pemakaian_bulan_tv = total_pemakaian_tv * 30
 total_pemakaian_bulan_kipas = total_pemakaian_kipas * 30
 total_pemakaian_bulan_lampu = total_pemakaian_lampu * 30
 
-print('total pemakaian  perbulan:', total_pemakaian_bulan_tv,'watt')
-print('total pemakaian  perbulan:', total_pemakaian_bulan_kipas,'watt')
-print('total pemakaian  perbulan:', total_pemakaian_bulan_lampu,'watt')
+print('total pemakaian  perbulan:', total_pemakaian_bulan_tv,'kwh')
+print('total pemakaian  perbulan:', total_pemakaian_bulan_kipas,'kwh')
+print('total pemakaian  perbulan:', total_pemakaian_bulan_lampu,'kwh')
 
-total_harga_pemakaian_tv= total_pemakaian_bulan_tv * harga 
-total_harga_pemakaian_kipas= total_pemakaian_bulan_kipas * harga 
-total_harga_pemakaian_lampu= total_pemakaian_bulan_lampu * harga  
 
-print("Total harga pemakaian: Rp.", total_harga_pemakaian_tv)
-print("Total harga pemakaian: Rp.", total_harga_pemakaian_kipas)
-print("Total harga pemakaian: Rp.", total_harga_pemakaian_lampu)
+
+
